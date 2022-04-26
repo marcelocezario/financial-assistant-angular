e498b9d5b518062bf64e7bcbe4b11dbc9caa57d9
Marcelo Cezario <marcelocezario@gmail.com>
2022-04-25 22:07:35 -0300

    Criando home page

--- financial-assistant/src/app/app.module.ts
+++ financial-assistant/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BodyComponent } from './components/template/body/body.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SectionTitleComponent } from './components/utils/section-title/section-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    HomePageComponent,
+    SectionTitleComponent
   ],
   imports: [
     BrowserModule,

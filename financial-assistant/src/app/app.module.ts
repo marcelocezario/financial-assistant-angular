c00ea27e47e8cc0bf17b035cfc4e3611a337e04b
Marcelo Cezario <marcelocezario@gmail.com>
2022-04-26 15:06:46 -0300

    Criado bottom-navigation-bar

--- financial-assistant/src/app/app.module.ts
+++ financial-assistant/src/app/app.module.ts
@@ -7,18 +7,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BodyComponent } from './components/template/body/body.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SectionTitleComponent } from './components/utils/section-title/section-title.component';
+import { BottomNavigationBarComponent } from './components/template/bottom-navigation-bar/bottom-navigation-bar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     BodyComponent,
     HomePageComponent,
-    SectionTitleComponent
+    SectionTitleComponent,
+    BottomNavigationBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]

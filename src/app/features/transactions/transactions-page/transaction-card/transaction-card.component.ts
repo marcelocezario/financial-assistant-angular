57723789f0bd2ef8a9e487cd9c73e94faa4010cd
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2024-05-25 12:00:21 -0300

    Ajustes transaction card

--- src/app/features/transactions/transactions-page/transaction-card/transaction-card.component.ts
+++ src/app/features/transactions/transactions-page/transaction-card/transaction-card.component.ts
@@ -5,11 +5,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
+import { AppCurrencyPipe } from '../../../../shared/pipes/app-currency.pipe';
 
 @Component({
   selector: 'app-transaction-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, CommonModule, MatChipsModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, CommonModule, MatChipsModule, MatIconModule, AppCurrencyPipe],
   templateUrl: './transaction-card.component.html',
   styleUrl: './transaction-card.component.scss'
 })

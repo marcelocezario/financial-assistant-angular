b5343484233847af98389e63cedf8cd8ed5f28fc
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2024-06-27 18:23:13 -0300

    Ajustando código tempo relativo

--- src/app/shared/pipes/app-relative-time.pipe.ts
+++ src/app/shared/pipes/app-relative-time.pipe.ts
@@ -1,4 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { LanguageService } from '../language';
+
+const timeConstants = Object.freeze({
+  SECONDS_IN_MILLISECONDS: 1000,
+  MINUTES_IN_MILLISECONDS: 60000,
+  HOURS_IN_MILLISECONDS: 3600000,
+  DAYS_IN_MILLISECONDS: 86400000,
+  WEEKS_IN_MILLISECONDS: 604800000,
+  MONTHS_IN_YEARS: 12
+});
+
+type TimeType = 'SECONDS' | 'MINUTES' | 'HOURS' | 'DAYS' | 'WEEKS' | 'MONTHS' | 'YEARS' | 'NOW' | 'TODAY' | 'YESTERDAY' | 'TOMORROW'
+type TemporalPosition = 'PAST' | 'FUTURE'
+type DayOfWeek = 'sunday' | 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday'
 
 @Pipe({
   name: 'appRelativeTime',
@@ -6,46 +20,117 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AppRelativeTimePipe implements PipeTransform {
 
+  constructor(private _languageService: LanguageService) { }
+
   transform(value: Date | string): unknown {
     if (!value) {
-      return value;
+      return value
     }
     try {
-      const now = new Date();
-      const date = new Date(value);
-      const diffInMs = now.getTime() - date.getTime();
-      const diffInSeconds = Math.floor(diffInMs / 1000);
-      const diffInMinutes = Math.floor(diffInSeconds / 60);
-      const diffInHours = Math.floor(diffInMinutes / 60);
-      const diffInDays = Math.floor(diffInHours / 24);
-      const diffInWeeks = Math.floor(diffInDays / 7);
-      const diffInMonths = Math.floor(diffInDays / 30);
-      const diffInYears = Math.floor(diffInDays / 365);
+      const date = new Date(value)
+      const now = new Date('2024-06-30T12:00:00')
+      const temporalPosition = now.getTime() >= date.getTime() ? 'PAST' : 'FUTURE'
+      const yesterday = new Date(now.getTime() - 86400000)
+      const tomorrow = new Date(now.getTime() + 86400000)
+      const diffInSeconds = this._getDiffDatesInSeconds(now, date)
+      const diffInMinutes = this._getDiffDatesInMinutes(now, date)
+      const diffInHours = this._getDiffDatesInHours(now, date)
+      const diffInDays = this._getDiffDatesInDays(now, date)
+      const diffInWeeks = this._getDiffDatesInWeeks(now, date)
+      const diffInMonths = this._getDiffDatesInMonths(now, date)
+      const diffInYears = this._getDiffDatesInYears(now, date)
 
       /**
        * TODO
-       * - adicionar suporte multi idiomas;
        * - adicionar datas futuras, ex: amanhã, semana que vem, etc...;
        * - adicionar maior especificidade, ex: 'Há 1 semana e 2 dias', 'Há 1 mês e 10 dias'
        */
 
-      if (diffInSeconds < 60) {
-        return 'agora';
+      if (yesterday.toDateString() === date.toDateString()) {
+        return this._getTranslate('YESTERDAY')
+      } else if (tomorrow.toDateString() === date.toDateString()) {
+        return this._getTranslate('TOMORROW')
+      } else if (diffInSeconds < 60) {
+        return this._getTranslate('NOW')
       } else if (diffInMinutes < 60) {
-        return `${diffInMinutes} minutos atrás`;
+        return this._getTranslate('MINUTES', diffInMinutes, temporalPosition)
       } else if (diffInHours < 24) {
-        return `${diffInHours} horas atrás`;
-      } else if (diffInDays < 7) {
-        return `${diffInDays} dias atrás`;
-      } else if (diffInWeeks < 4) {
-        return `${diffInWeeks} semanas atrás`;
+        return this._getTranslate('HOURS', diffInHours, temporalPosition)
+      } else if (now.toDateString() === date.toDateString()) {
+        return this._getTranslate('TODAY')
+      } else if (diffInDays < 6) {
+        if (temporalPosition === 'PAST') {
+          return `${this._getDaysOfTheWeek(date)}`
+        }
+        return this._getTranslate('DAYS', diffInDays, temporalPosition)
       } else if (diffInMonths < 12) {
-        return `${diffInMonths} meses atrás`;
+        if (diffInMonths < 1) {
+          return this._getTranslate('WEEKS', diffInWeeks, temporalPosition)
+        }
+        return this._getTranslate('MONTHS', diffInMonths, temporalPosition)
       } else {
-        return `${diffInYears} anos atrás`;
+        return this._getTranslate('YEARS', diffInYears, temporalPosition)
       }
     } catch (_) {
       return value;
+    }
+  }
+
+  private _getTranslate(key: TimeType, count: number | undefined = undefined, temporalPosition: TemporalPosition | undefined = undefined): string {
+    let translateKey = `generic.relativeTime.${key}`
+    if (count) {
+      translateKey = `${translateKey}.${count === 1 ? 'singular' : 'plural'}`
+    }
+    const translated = this._languageService.getTranslateInstant(translateKey, { count: count })
+    if (temporalPosition) {
+      return this._languageService.getTranslateInstant(`generic.relativeTime.${temporalPosition}`, { relativeTime: translated })
+    }
+    return translated
+  }
+
+  private _getTranslateDayOfWeek(dayOfWeek: DayOfWeek): string {
+    const translateKey = `generic.relativeTime.DAY_OF_WEEK.${dayOfWeek}`
+    return this._languageService.getTranslateInstant(translateKey)
+  }
+
+  private _getDiffDatesInSeconds(date1: Date, date2: Date): number {
+    return Math.floor(Math.abs((date1.getTime() - date2.getTime()) / timeConstants.SECONDS_IN_MILLISECONDS))
+  }
+
+  private _getDiffDatesInMinutes(date1: Date, date2: Date): number {
+    return Math.floor(Math.abs((date1.getTime() - date2.getTime()) / timeConstants.MINUTES_IN_MILLISECONDS))
+  }
+
+  private _getDiffDatesInHours(date1: Date, date2: Date): number {
+    return Math.floor(Math.abs((date1.getTime() - date2.getTime()) / timeConstants.HOURS_IN_MILLISECONDS))
+  }
+
+  private _getDiffDatesInDays(date1: Date, date2: Date): number {
+    return Math.floor(Math.abs((date1.getTime() - date2.getTime()) / timeConstants.DAYS_IN_MILLISECONDS))
+  }
+
+  private _getDiffDatesInWeeks(date1: Date, date2: Date): number {
+    return Math.floor(Math.abs((date1.getTime() - date2.getTime()) / timeConstants.WEEKS_IN_MILLISECONDS))
+  }
+
+  private _getDiffDatesInMonths(date1: Date, date2: Date): number {
+    return Math.floor(Math.abs(date1.getUTCMonth() - date2.getUTCMonth() + (this._getDiffDatesInYears(date1, date2) * timeConstants.MONTHS_IN_YEARS)))
+  }
+
+  private _getDiffDatesInYears(date1: Date, date2: Date): number {
+    return Math.floor(Math.abs(date1.getUTCFullYear() - date2.getUTCFullYear()))
+  }
+
+  private _getDaysOfTheWeek(date: Date): string {
+    switch (date.getDay()) {
+      case 0: return this._getTranslateDayOfWeek(`sunday`)
+      case 1: return this._getTranslateDayOfWeek(`monday`)
+      case 2: return this._getTranslateDayOfWeek(`tuesday`)
+      case 3: return this._getTranslateDayOfWeek(`wednesday`)
+      case 4: return this._getTranslateDayOfWeek(`thursday`)
+      case 5: return this._getTranslateDayOfWeek(`friday`)
+      case 6: return this._getTranslateDayOfWeek(`saturday`)
+      default: return date.toDateString()
     }
   }
 

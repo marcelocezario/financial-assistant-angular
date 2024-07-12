21cb7f5bc70304829c3b725c754f490641547d09
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2024-07-11 21:24:34 -0300

    Add formulário de categorias de transação

--- src/app/features/transactions/transactions-form-page/transactions-form-page.component.ts
+++ src/app/features/transactions/transactions-form-page/transactions-form-page.component.ts
@@ -1,35 +1,40 @@
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Category, Transaction, TransactionCategory, ClassificationType, Wallet } from '../../../core/models';
 import { CategoryService } from '../../categories';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { DialogService, FormBaseDirective, InputComponent, InputDatetimeComponent, LanguageService, NotificationService, SelectComponent, StorageService, Utils } from '../../../shared';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleChange, MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { ROUTES_KEYS } from '../../../core/config';
 import { TransactionService } from '../transaction.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { WalletService } from '../../wallets';
-import Decimal from 'decimal.js';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-transactions-form-page',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
     InputComponent,
     InputDatetimeComponent,
     MatButtonModule,
     MatButtonToggleModule,
     MatCardModule,
     MatCheckboxModule,
+    MatDividerModule,
     MatIconModule,
+    MatSlideToggleModule,
     MatSortModule,
     MatTableModule,
     ReactiveFormsModule,
@@ -41,16 +46,9 @@ import Decimal from 'decimal.js';
 })
 export class TransactionsFormPageComponent extends FormBaseDirective implements OnInit {
 
-  categories: Category[] = []
+  userCategories: Category[] = []
   wallets: Wallet[] = []
   transactionTypes = Object.keys(ClassificationType)
-  categoriesOptions: Category[] = []
-  categoriesAmount: number = 0
-
-  formCategories: FormGroup<any> = this._formBuilder.group({
-    category: [null, [Validators.required]],
-    amount: [null, [Validators.required, Validators.min(0)]]
-  })
 
   override formGroup: FormGroup<any> = this._formBuilder.group({
     id: [null],
@@ -66,7 +64,7 @@ export class TransactionsFormPageComponent extends FormBaseDirective implements 
     updatedAt: [null],
     userId: [null],
     wallet: [null],
-    categories: this._formBuilder.array([])
+    categories: this._formBuilder.array([this._buildCategory()])
   })
 
   constructor(
@@ -84,13 +82,43 @@ export class TransactionsFormPageComponent extends FormBaseDirective implements 
     super();
   }
 
+  get categories(): FormArray {
+    return this.formGroup.get('categories') as FormArray;
+  }
+
+  get transactionType(): ClassificationType | undefined {
+    return this.formGroup?.get('type')?.value
+  }
+
+  get categoriesAmount(): number {
+    const categories = this.categories
+    const value = categories.controls
+      .map(control => control.value)
+      .map(transactionCategory => {
+        if (!transactionCategory) {
+          return 0
+        }
+        if (transactionCategory.category === ClassificationType.EXPENSE) {
+          return transactionCategory.amount * -1
+        }
+        return transactionCategory.amount
+      })
+      .reduce((a, b) => a + b, 0.0)
+    return value
+  }
+
+  getCategoriesOptions(discountCategory: boolean = false): Category[] {
+    return this.userCategories.filter(c => (c.type === this.transactionType) !== discountCategory) || []
+  }
+
+
   async ngOnInit(): Promise<void> {
     const [categories, wallets, transaction] = await Promise.all([
       this._categoryService.getByUser(),
       this._walletService.getByUser(),
       this._findTransactionById()
     ]);
-    this.categories = categories;
+    this.userCategories.push(...categories);
     this.wallets = wallets;
   }
 
@@ -105,11 +133,17 @@ export class TransactionsFormPageComponent extends FormBaseDirective implements 
             this.wallets.unshift(transaction.wallet)
           }
           const categories = transaction.categories || []
-          const categoriesFormArray = this.formGroup.get('categories') as FormArray;
-          categories.forEach(category => {
-            categoriesFormArray.push(this._formBuilder.control(category));
+          const categoriesFormArray = this.categories
+          this.removeCategory(0)
+          categories.forEach((category, index) => {
+            // if (index !== 0) {
+            //   categoriesFormArray.push(this._buildCategory(category));
+            // }
+            categoriesFormArray.push(this._buildCategory(category));
+            if (category.category.active === false) {
+              this.userCategories.push(category.category)
+            }
           });
-          this.changeTransactionType(transaction.type)
         })
         .catch(() => this._router.navigate([`/${ROUTES_KEYS.transactions}`]))
     }
@@ -123,6 +157,22 @@ export class TransactionsFormPageComponent extends FormBaseDirective implements 
     } else {
       await this._create(transaction).then();
     }
+  }
+
+  override cancel(): void {
+    this._router.navigate([`/${ROUTES_KEYS.transactions}`])
+  }
+
+  private _buildCategory(category: TransactionCategory | undefined = undefined): FormGroup {
+    let isDiscount: boolean = false
+    if (this.transactionType && category) {
+      isDiscount = this.transactionType !== category?.category.type
+    }
+    return this._formBuilder.group({
+      category: [category?.category, [Validators.required]],
+      amount: [category?.amount, [Validators.required, Validators.min(0)]],
+      isDiscount: [isDiscount]
+    });
   }
 
   private async _create(transaction: Transaction) {
@@ -145,104 +195,16 @@ export class TransactionsFormPageComponent extends FormBaseDirective implements 
     })
   }
 
-  override cancel(): void {
-    this._router.navigate([`/${ROUTES_KEYS.transactions}`])
+  convertAbstractControlToFormControl(control: AbstractControl<any, any> | null): FormControl {
+    return control as FormControl
   }
 
-  async deleteTransactionCategory(transactionCategory: TransactionCategory) {
-    const titleKey = this.getTranslateKey('transactionCategoryDeleteConfirmTitle')
-    const messageKey = this.getTranslateKey('transactionCategoryDeleteConfirmMessage')
-    const translated = await this._languageService.getTranslate([titleKey, messageKey], { categoryName: transactionCategory.category.name })
-    const userConfirm = await this._dialogService.openConfirmation({ title: translated[titleKey], message: translated[messageKey] }).then() || false
-    if (userConfirm) {
-      const successfullyMessageKey = this.getTranslateKey('transactionCategoryDeletedSuccessfully')
-      const successfullyMessage = await this._languageService.getTranslate(successfullyMessageKey, { categoryName: transactionCategory.category.name }).then()
-      this._notification.warning(successfullyMessage)
-      const categoriesArray = this.formGroup.get('categories') as FormArray
-      const index = categoriesArray.controls.findIndex((control) => control.value.category.id === transactionCategory.category.id)
-      if (index !== -1) {
-        categoriesArray.removeAt(index)
-      }
-      this.updateCategoriesAmountDiff()
-    }
+  addCategory() {
+    this.categories.push(this._buildCategory());
   }
 
-  getCategoriesAmount(): number {
-    const transactionType = this.formGroup.get('type')?.value
-    const transactionCategories = this.formGroup.get('categories')?.value as Array<TransactionCategory> || []
-    const amount = transactionCategories.map((c: TransactionCategory) => {
-      if (transactionType) {
-        return transactionType === c.category.type ? c.amount : c.amount * -1
-      }
-      return c.category.type === ClassificationType.INCOME ? c.amount : c.amount * -1
-    }).reduce((a: number, b: number) => (a * 1) + (b * 1), 0.0)
-    return transactionType ? amount : Math.abs(amount)
-  }
-
-  async addCategory(formCategories: FormGroup) {
-    if (!formCategories.get('type')?.value) {
-      const transactionType = this.formGroup.get('type')?.value
-      if (!transactionType) {
-        const messageKey = this.getTranslateKey('transactionTypeError')
-        const message = await this._languageService.getTranslate(messageKey).then()
-        this._notification.error(message)
-        return
-      }
-      formCategories.get('type')?.setValue(transactionType)
-    }
-    if (!formCategories.valid) {
-      return
-    }
-    const formArray = this.formGroup.get('categories') as FormArray
-    const transactionCategory: TransactionCategory = formCategories.value
-
-    const existingIndex = formArray.controls.findIndex((control) => {
-      const category = control.value as TransactionCategory
-      return category.category.id === transactionCategory.category.id
-    })
-
-    if (existingIndex >= 0) {
-      const titleKey = this.getTranslateKey('mergeCategoryTitle')
-      const messageKey = this.getTranslateKey('mergeCategoryMessage')
-      const translated = await this._languageService.getTranslate([titleKey, messageKey], { categoryName: transactionCategory.category.name }).then()
-      const sumDuplicateCategories = await this._dialogService.openConfirmation({ title: translated[titleKey], message: translated[messageKey] }).then()
-      if (!sumDuplicateCategories) {
-        return
-      }
-      const existingAmount = new Decimal((formArray.controls[existingIndex].value as TransactionCategory).amount)
-      const newAmount = new Decimal(transactionCategory.amount);
-      (formArray.controls[existingIndex].value as TransactionCategory).amount = existingAmount.add(newAmount).toNumber()
-    } else {
-      formArray.push(this._formBuilder.control(transactionCategory))
-    }
-    formCategories.reset()
-    this.updateCategoriesAmountDiff();
-  }
-
-  getCategoriesDiff(): number {
-    const transactionAmount = new Decimal(this.formGroup.get('amount')?.value || 0)
-    const categoriesAmount = new Decimal(this.getCategoriesAmount())
-    const diff = transactionAmount.minus(categoriesAmount).toNumber()
-    return diff
-  }
-
-  updateCategoriesAmountDiff() {
-    this.categoriesAmount = this.getCategoriesAmount()
-    const diff = new Decimal(this.getCategoriesDiff())
-    const transactionType = this.formGroup.get('type')?.value
-    if (transactionType) {
-      if (diff.toNumber() < 0) {
-        this.formCategories.get('type')?.setValue(transactionType === ClassificationType.INCOME ? ClassificationType.EXPENSE : ClassificationType.INCOME)
-      } else {
-        this.formCategories.get('type')?.setValue(transactionType === ClassificationType.INCOME ? ClassificationType.INCOME : ClassificationType.EXPENSE)
-      }
-    }
-    this.formCategories.get('amount')?.setValue((diff && !diff.equals(0)) ? Math.abs(diff.toNumber()) : null);
-    this.formGroup.markAsDirty();
-  }
-
-  changeTransactionType(transactionType: string) {
-    this.categoriesOptions = this.categories.filter(c => c.type === transactionType)
+  removeCategory(index: number) {
+    this.categories.removeAt(index);
   }
 
 }
